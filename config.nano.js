6898b0a602a2e0ff214d2129d40c63977e3b6ee5
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-08-05 12:21:15 -0600

    add new patch

--- config.nano.js
+++ config.nano.js
@@ -71,6 +71,7 @@
             "./patches/20_2_hard_purge_any_key.patch",
             "./patches/20_3_reset_hard.patch",
             "./patches/20_4_log_script_snippet_errors.patch",
+            "./patches/20_5_whitelist_style.patch",
 
             "./patches/30_0_link_resources.patch",
 
