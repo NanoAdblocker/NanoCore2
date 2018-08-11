524ffd1a2a862751bb6c06672710a4d84af22047
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-08-11 13:14:30 -0600

    new draft

--- term/crowdin.js
+++ term/crowdin.js
@@ -30,7 +30,9 @@
  * Modules.
  * @const {Module}
  */
+const fs = require("fs-extra");
 const https = require("https");
+const path = require("path");
 const url = require("url");
 const yauzl = require("yauzl");
 
@@ -60,21 +62,81 @@ const stream_to_buffer = (stream, on_done, on_error) => {
 
 /**
  * Download a file.
- * @async @function
+ * @function
  * @param {string} link - URL to the file to download. Must be using HTTPS
  * protocol.
- * @returns {Buffer} File content.
- * @throws When things go wrong.
+ * @param {Function} on_done - Done event handler.
+ *     @param {string} file - File content.
+ * @param {Function} on_error - Error event handler.
+ *     @param {Error} err - Error object.
  */
-const download = (link) => {
-    return new Promise((resolve, reject) => {
-        const opt = url.parse(link);
-        const req = https.request(opt, (res) => {
-            stream_to_buffer(res, resolve, reject);
-        });
-        req.on("error", reject);
-        req.end();
+const download = (link, on_done, on_error) => {
+    const opt = url.parse(link);
+
+    const req = https.request(opt, (res) => {
+        if (
+            res.statusCode === 301 ||
+            res.statusCode === 302 ||
+            res.statusCode === 307 ||
+            res.statusCode === 308
+        ) {
+            const new_link = res.headers["location"];
+            if (
+                typeof new_link !== "string" ||
+                !new_link.startsWith("https://")
+            ) {
+                on_error(new Error("Bad server response."));
+            } else {
+                download(new_link, on_done, on_error);
+            }
+            return;
+        }
+
+        if (res.statusCode !== 200) {
+            on_error(new Error("Connection error."));
+            return;
+        }
+
+        stream_to_buffer(res, on_done, on_error);
     });
+
+    req.on("error", on_error);
+    req.end();
+};
+
+/**
+ * Validate JSON stream and write to a file.
+ * @function
+ * @param {string} file - Path to output file.
+ * @param {ReadableStream} stream - Data stream.
+ * @param {Function} on_done - Done event handler.
+ * @param {Function} on_error - Error event handler.
+ *     @param {Error} err - Error object.
+ */
+const validated_write = (file, stream, on_done, on_error) => {
+    let data = "";
+
+    stream.setEncoding("utf8");
+    stream.on("data", (c) => {
+        data += c;
+    });
+
+    stream.on("end", () => {
+        try {
+            data = JSON.parse(data);
+        } catch (err) {
+            on_error(err);
+            return;
+        }
+
+        fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
+            if (err)
+                on_error(err);
+            else
+                on_done();
+        });
+    });
+    stream.on("error", on_error);
 };
 
 /*****************************************************************************/
@@ -93,29 +155,67 @@ const locales = new Map([
 /*****************************************************************************/
 
 /**
- * Download, validate, unpack, and synchronize supported non-English locales
- * with the Crowdin project.
+ * Download, unpack, and synchronize supported non-English locales with the
+ * Crowdin project.
+ * Note that you still have to manually build the project on Crowdin.
  * @async @function
  * @throws When things go wrong.
  */
 exports.sync = async () => {
-    const file = download(
-        "https://crowdin.com/backend/download/project/nano-adblocker.zip",
-    );
+    const file = await new Promise((resolve, reject) => {
+        download(
+            "https://crowdin.com/backend/download/project/nano-adblocker.zip",
+            resolve, reject,
+        );
+    });
 
     await new Promise((resolve, reject) => {
         const done = new Set();
 
-        yauzl.fromBuffer(file, (err, zip) => {
+        yauzl.fromBuffer(file, { lazyEntries: true }, (err, zip) => {
             if (err) {
                 reject(err);
                 return;
             }
 
-            zip.on("entry", (entry) => {
-                console.log(entry.fileName);
+            const next = () => {
                 zip.readEntry();
+            };
+
+            zip.on("entry", (entry) => {
+                const name = entry.fileName;
+                if (!name.includes("/") || name.endsWith("/"))
+                    return;
+
+                const [key, messages] = name.split("/");
+                if (messages !== "messages.json")
+                    return;
+                if (!locales.has(key))
+                    return;
+
+                const norm_key = locales.get(key);
+
+                const outdir = path.resolve("./src/_locales", norm_key);;
+                fs.mkdirp(outdir, (err) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    zip.openReadStream(entry, (err, stream) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+
+                        validated_write(
+                            path.resolve(outdir, "messages.json"),
+                            stream, next, reject,
+                        );
+                    });
+                });
             });
+
             zip.on("end", () => {
                 if (locales.size !== done.size)
                     reject(new Error("Some locale files are missing."));
@@ -124,7 +224,7 @@ exports.sync = async () => {
             });
             zip.on("error", reject);
 
-            zip.readEntry();
+            next();
         });
     });
 };
