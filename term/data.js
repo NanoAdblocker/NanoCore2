ae8b57de097f712cd33d24532f6eb42647b1f3fe
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-05-10 22:52:09 -0600

    publish filters & locale update

--- term/data.js
+++ term/data.js
@@ -30,7 +30,7 @@ const assert = require("assert");
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
-exports.version = "1.0.0.106";
+exports.version = "1.0.0.107";
 
 exports.based_on = [
     "uBlock Origin",
