4ef76055e1fff22bf2688100e7bcc45ff80ccf59
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-05-11 11:54:02 -0600

    refactor

--- term/index.js
+++ term/index.js
@@ -1,35 +1,35 @@
-/******************************************************************************
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    Nano Core 2 - An adblocker
-    Copyright (C) 2018  Nano Core 2 contributors
+// Nano Core 2 - An adblocker
+// Copyright (C) 2018-2019  Nano Core 2 contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+// Entry point of Nano Core 2 Terminal
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*******************************************************************************
-
-    Entry point of Nano Core 2 Terminal.
-
-******************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 "use strict";
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Modules.
- * @const {Module}
- */
+const APP_NAME = "Nano Core 2 Terminal";
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 const assert = require("assert");
 const build = require("./build.js");
 const crowdin = require("./crowdin.js");
@@ -37,27 +37,16 @@ const fs = require("fs-extra");
 const os = require("os");
 const path = require("path");
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Terminal instance.
- * @const {Term}
- */
-const term = new (require("./term.js"))();
-
-/**
- * System busy flag.
- * @var {boolean}
- */
 let busy = true;
 
-/**
- * Execute a command and validate exit code.
- * @async @function
- * @param {integer} expected - The expected exit code.
- * @param {Any} ...args - Arguments for term.exec().
- * @throws When exit code does not match expectation.
- */
+// ----------------------------------------------------------------------------------------------------------------- //
+
+const term = new (require("./term.js"))();
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
 const exec = async (expected, ...args) => {
     assert(typeof expected === "number");
 
@@ -66,34 +55,19 @@ const exec = async (expected, ...args) => {
         throw new Error("Exit code is not " + expected.toString() + ".");
 };
 
-/**
- * Asynchronously wait for a given amount of time.
- * @async @function
- * @param {integer} delay - Time to wait, in milliseconds.
- */
 const sleep = (delay) => {
     return new Promise((resolve) => {
         setTimeout(resolve, delay);
     });
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Configuration object.
- * @object
- * {Array.<string>} Patches - Patches, in order.
- * {string} Source - Original directory.
- * {string} Target - Development directory.
- * {string} Output - Patch output.
- */
+// Source - Path to original directory
+// Target - Path to development directory
+// Output - Path to patch output
 const config = {};
 
-/**
- * Load or reload configuration file.
- * @async @function
- * @throws When configuration file could not be opened or is invalid.
- */
 const config_load = async () => {
     const data = eval(await fs.readFile("./config.nano.js", "utf8"));
 
@@ -123,68 +97,47 @@ const config_load = async () => {
     build.src_repo = config.Target;
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Command handlers registry.
- * @var {Map}
- */
 const cmd_handlers = new Map();
 
 term.set_listener((cmd) => {
     assert(typeof cmd === "string");
 
-    if (busy)
+    if (busy) {
         term.write_line("Error: System busy.");
-    else if (cmd.length === 0)
+    } else if (cmd.length === 0) {
         term.ready();
-    else if (cmd_handlers.has(cmd))
-        (cmd_handlers.get(cmd))();
-    else
+    } else if (cmd_handlers.has(cmd)) {
+        const handler = cmd_handlers.get(cmd);
+        handler();
+    } else {
         term.write_line("Error: Unknown command.").ready();
+    }
 });
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Default options for executing commands.
- * Return value may change when the configuration file is reloaded.
- * @function
- * @return {Option} Default options.
- */
+// Default exec options
 const exec_opt = () => {
     return {
         cwd: config.Target,
     };
 };
 
-/**
- * Apply a patch.
- * @async @function
- * @param {string} p - Path to the patch.
- * @throws When things go wrong.
- */
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// Apply patch
 const apply = async (p) => {
     term.write_line("Applying " + p);
     await exec(0, "git", exec_opt(), "apply", p);
 };
 
-/**
- * Commit current changes.
- * @async @function
- * @throws When things go wrong.
- */
 const commit = async () => {
     await exec(0, "git", exec_opt(), "add", "-A");
     await exec(0, "git", exec_opt(), "commit", "-m", "Apply patches");
 };
 
-/**
- * Create a patch and write it to a fil.
- * @async @function
- * @param {string} p - Path to the file.
- * @throws When things go wrong.
- */
 const diff = async (p) => {
     let has_error = false;
 
@@ -214,7 +167,7 @@ const diff = async (p) => {
     });
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 cmd_handlers.set("reset", async () => {
     busy = true;
@@ -283,105 +236,57 @@ cmd_handlers.set("mark", async () => {
     term.ready();
 });
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * All supported browsers.
- * @const {Array.<string>}
- */
 const browsers = [
     "chromium",
     // "edge",
 ];
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Build extension package for one browser.
- * @async @function
- * @param {string} b - Browser name.
- * @throws When things go wrong.
- */
-const make_one = async (b) => {
-    await build.build_core(b);
-    await build.build_filters(b);
-    await build.build_resources(b);
-    await build.build_locale(b);
+const make_one = async (browser) => {
+    await build.build_core(browser);
+    await build.build_filters(browser);
+    await build.build_resources(browser);
+    await build.build_locale(browser);
 };
 
-/**
- * Test build output for one browser.
- * @async @function
- * @param {string} b - Browser name.
- * @throws When things go wrong.
- */
-const test_one = async (b) => {
-    await build.test(b);
+const test_one = async (browser) => {
+    await build.test(browser);
 };
 
-/**
- * Create ZIP package for one browser.
- * @async @function
- * @param {string} b - Browser name.
- * @throws When things go wrong.
- */
-const pack_one = async (b) => {
-    await build.pack(b);
+const pack_one = async (browser) => {
+    await build.pack(browser);
 };
 
-/**
- * Publish package for one browser to extension store.
- * @async @function
- * @param {string} b - Browser name.
- * @throws When things go wrong.
- */
-const publish_one = async (b) => {
-    await build.publish(b, term);
+const publish_one = async (browser) => {
+    await build.publish(browser, term);
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Build extension package for all supported browsers.
- * @async @function
- * @throws When things go wrong.
- */
 const make = async () => {
     for (const b of browsers)
         await make_one(b);
 };
 
-/**
- * Test build output for all supported browsers.
- * @async @function
- * @throws When things go wrong.
- */
 const test = async () => {
     for (const b of browsers)
         await test_one(b);
 };
 
-/**
- * Create ZIP package for all supported browsers.
- * @async @function
- * @throws When things go wrong.
- */
 const pack = async () => {
     for (const b of browsers)
         await pack_one(b);
 };
 
-/**
- * Publish package for all supported browsers to extension store.
- * @async @function
- * @throws When things go wrong.
- */
 const publish = async () => {
     for (const b of browsers)
         await publish_one(b);
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 cmd_handlers.set("make", async () => {
     busy = true;
@@ -458,13 +363,8 @@ cmd_handlers.set("clean", async () => {
     term.ready();
 });
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
-/**
- * Build English locale file from locale declaration.
- * @async @function
- * @throws When things go wrong.
- */
 const lmake = async () => {
     const data = eval(await fs.readFile("./locale.nano.js", "utf8"));
 
@@ -478,18 +378,11 @@ const lmake = async () => {
     );
 };
 
-/**
- * Synchronize (non-English) locale files with the latest build of the
- * Crowdin project.
- * This will not rebuild the Crowdin project even if there are changes.
- * @async @function
- * @throws When things go wrong.
- */
 const lsync = async () => {
     await crowdin.sync();
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 cmd_handlers.set("lmake", async () => {
     busy = true;
@@ -517,7 +410,7 @@ cmd_handlers.set("lsync", async () => {
     term.ready();
 });
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 cmd_handlers.set("config", () => {
     term.write_line(JSON.stringify(config, null, 2));
@@ -538,22 +431,24 @@ cmd_handlers.set("exit", () => {
     term.destructor();
 });
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 process.on("unhandledRejection", (err) => {
     throw err;
 });
 
-{
-    const name = "Nano Core 2 Terminal";
-    term.title(name).write_line(name);
-}
 
-(async () => {
+term.title(APP_NAME).write_line(APP_NAME);
+
+const main = async () => {
     await config_load();
 
     busy = false;
     term.ready();
-})();
+};
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
+
+main();
+
+// ----------------------------------------------------------------------------------------------------------------- //
