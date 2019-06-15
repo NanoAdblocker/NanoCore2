00bc186dfc6e8a6bf07dd3789bf8d2bf222d17a3
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-06-14 23:59:00 -0600

    remove legacy patch

--- config.nano.js
+++ config.nano.js
@@ -73,7 +73,6 @@
             "./patches/20_3_reset_hard.patch",
             "./patches/20_4_log_script_snippet_errors.patch",
             "./patches/20_5_whitelist_style.patch",
-            "./patches/20_6_warn_unsaved.patch",
             "./patches/20_7_autocomment_href.patch",
 
             "./patches/30_0_link_resources.patch",
