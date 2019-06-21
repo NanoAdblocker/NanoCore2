eb285e0a173f9c32b6c55331c4e97a416dc39c54
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-06-20 21:21:54 -0600

    publish stable build

--- term/data.js
+++ term/data.js
@@ -30,7 +30,7 @@ const assert = require("assert");
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
-exports.version = "1.0.0.117";
+exports.version = "1.0.0.118";
 
 exports.based_on = [
     "uBlock Origin",
