c69354795dc273fa06c6c1e7e2d034b5e236953c
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-07-20 23:32:19 -0600

    refactor

--- src/js/nano-asset-viewer.js
+++ src/js/nano-asset-viewer.js
@@ -1,39 +1,40 @@
-/******************************************************************************
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    Nano Core 2 - An adblocker
-    Copyright (C) 2018  Nano Core 2 contributors
+// Nano Core 2 - An adblocker
+// Copyright (C) 2018-2019  Nano Core 2 contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+// Asset viewer script
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*******************************************************************************
-
-    Asset viewer script.
-
-******************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 "use strict";
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 var nano = nano || {};
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nano.active_asset = "";
+
 nano.editor = new nano.Editor("content", true, true);
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nano.load_settings = () => {
     const on_msg = (wrap) => {
@@ -44,19 +45,20 @@ nano.load_settings = () => {
     vAPI.messaging.send(
         "dashboard",
         {
-            what: "userSettings",
             name: "nanoViewerSoftWrap",
+            what: "userSettings",
         },
-        on_msg
+        on_msg,
     );
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nano.render_filter = () => {
     const on_msg = (details) => {
         if (details && details.content) {
             let content = details.content;
+
             if (!content.endsWith("\n"))
                 content += "\n";
 
@@ -67,9 +69,9 @@ nano.render_filter = () => {
             if (nano.active_asset !== "user-filters") {
                 const anno = [
                     {
-                        row: 0,
-                        type: "error",
                         text: vAPI.i18n("nano_v_read_error"),
+                        type: "error",
+                        row: 0,
                     }
                 ];
                 nano.editor.set_anno(anno);
@@ -83,15 +85,15 @@ nano.render_filter = () => {
         vAPI.messaging.send(
             "default",
             {
-                what: "getAssetContent",
                 url: nano.active_asset,
+                what: "getAssetContent",
             },
-            on_msg
+            on_msg,
         );
     }
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nano.init = () => {
     const url = new URL(document.location);
@@ -104,8 +106,8 @@ nano.init = () => {
     nano.load_settings();
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nano.init();
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
--- src/js/nano-vapi.js
+++ src/js/nano-vapi.js
@@ -1,34 +1,34 @@
-/******************************************************************************
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    Nano Core 2 - An adblocker
-    Copyright (C) 2018  Nano Core 2 contributors
+// Nano Core 2 - An adblocker
+// Copyright (C) 2018-2019  Nano Core 2 contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+// ----------------------------------------------------------------------------------------------------------------- //
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+// Platform binding
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*******************************************************************************
-
-    nAPI binding.
-
-******************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 "use strict";
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 var nAPI = nAPI || {};
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
 
 nAPI.add_public_api_handler = (trusted, handler) => {
     chrome.runtime.onMessageExternal.addListener((msg, sender, res) => {
@@ -37,6 +37,7 @@ nAPI.add_public_api_handler = (trusted, handler) => {
 
         if (!trusted(sender.id))
             return;
+
         if (!handler(msg))
             return;
 
@@ -45,4 +46,4 @@ nAPI.add_public_api_handler = (trusted, handler) => {
     });
 };
 
-/*****************************************************************************/
+// ----------------------------------------------------------------------------------------------------------------- //
