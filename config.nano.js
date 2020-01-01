08d19889fc57f1dbd8c4112250b3a1a3171f4f72
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-12-31 18:44:12 -0700

    add temporary patch

--- config.nano.js
+++ config.nano.js
@@ -79,6 +79,7 @@
             "./patches/30_0_link_resources.patch",
 
             "./patches/90_0_temporary_migration.patch",
+            "./patches/90_1_temporary_bug_fix.patch",
 
             // ----------------------------------------------------------------------------------------------------- //
 
