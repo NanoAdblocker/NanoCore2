1a62f708f6b38a97cf993ebf8818625f4fb97990
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-05-21 08:53:57 -0600

    bump version

--- term/data.js
+++ term/data.js
@@ -30,12 +30,12 @@ const assert = require("assert");
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
-exports.version = "1.0.0.146";
+exports.version = "1.0.0.147";
 
 exports.based_on = [
     "uBlock Origin",
-    "Version/1.27.4",
-    "Commit/79c3f69",
+    "Version/1.27.6",
+    "Commit/d913337",
     "UserCSS/disabled",
 ].join(" ");
 
