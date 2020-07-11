f7da559591451e4cce06a9c734223871c7bb422b
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2020-07-11 12:09:25 -0600

    draft

--- config.nano.js
+++ config.nano.js
@@ -84,7 +84,8 @@
             "./patches/30_0_link_resources.patch",
 
             "./patches/90_0_assets_migration.patch",
-            "./patches/90_1_cherry_pick_issue_348.patch",
+            "./patches/90_1_cherry_pick_patches.patch",
+            "./patches/90_2_cherry_pick_script_snippets.patch",
 
             // ----------------------------------------------------------------------------------------------------- //
 
